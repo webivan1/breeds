12de7f24b3dbb23e7afe9569d2def0049a43a951
Ivan Maltsev <i.malcev@mediapronet.ru>
2022-11-21 00:49:39 +0000

    main - except warning - is not configured to support act

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,12 +2,13 @@ import { expect, afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
 import matchers from '@testing-library/jest-dom/matchers'
 
-// Object.defineProperty(global, 'IS_REACT_ACT_ENVIRONMENT', {
-//   // get: () => true,
-//   // set: () => {
-//   //   // return nothing
-//   // },
-// })
+// @todo temporary except warnings: The current testing environment is not configured to support act
+Object.defineProperty(global, 'IS_REACT_ACT_ENVIRONMENT', {
+  get: () => true,
+  set: () => {
+    // return nothing
+  },
+})
 
 // extends Vitest's expect method with methods from react-testing-library
 expect.extend(matchers)
